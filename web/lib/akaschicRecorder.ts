6065b445865b5d4e7173860ed4d935a16edc4a4e
avcdsld <avcdsld@gmail.com>
2022-03-13 19:30:46 +0900

    change sample data

--- web/lib/akaschicRecorder.ts
+++ web/lib/akaschicRecorder.ts
@@ -23,7 +23,7 @@ class AkaschicRecorder {
                         event_id: 1,
                         start_time: "2022-03-09T18:13:00Z",
                         end_time: "2022-03-09T18:15:00Z",
-                        event_name: "Event 1",
+                        event_name: "Sample Event 1",
                         rank_num: 1,
                         wallet_address: "0xe996FE17B655CC6830c3319002B71AF1Fb3ceCd6",
                         cid: "dummyeiefxy4xbf34rivlekencrtucworpv5jgnyrmkcpniclgjwk5m4jla"
@@ -32,7 +32,7 @@ class AkaschicRecorder {
                         event_id: 2,
                         start_time: "2022-03-09T18:16:00Z",
                         end_time: "2022-03-09T18:17:00Z",
-                        event_name: "Event 2",
+                        event_name: "Sample Event 2",
                         rank_num: 2,
                         wallet_address: "0xe996FE17B655CC6830c3319002B71AF1Fb3ceCd6",
                         cid: "dummyeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi"
@@ -41,7 +41,7 @@ class AkaschicRecorder {
                         event_id: 3,
                         start_time: "2022-03-09T18:18:00Z",
                         end_time: "2022-03-09T18:19:00Z",
-                        event_name: "Event 3",
+                        event_name: "Sample Event 3",
                         rank_num: 3,
                         wallet_address: "0xe996FE17B655CC6830c3319002B71AF1Fb3ceCd6",
                         cid: "dummyeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi"
